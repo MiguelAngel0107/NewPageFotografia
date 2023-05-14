10cd26285af805149751472ab27eac4429e20e96
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-05-13 20:15:28 -0500

    Update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "tailwindui.com",
+      "bafybeicgamofiuvkc6wjxl4wwzzh6pdovhcvvyc2gw5verruiolnykzz3i.ipfs.w3s.link",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
